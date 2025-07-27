bcb74f7efbfefc4238dd949313534f0a0d1772d7
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-07-27 21:29:13 +0200

    problem statement

---
+++ src/main/java/Aggregation.java
@@ -0,0 +1,15 @@
+/*
+Problem 2: Account Balance Aggregation
+
+Statement: Given a list of account transactions [{accountId: string, amount: number, type: "DEPOSIT" | "WITHDRAWAL"}], compute the final balance for each account. Ensure withdrawals don’t exceed the current balance (return -1 for invalid cases).
+Example:
+
+Input: transactions = [
+  {accountId: "A1", amount: 1000, type: "DEPOSIT"},
+  {accountId: "A1", amount: 500, type: "WITHDRAWAL"},
+  {accountId: "A2", amount: 200, type: "WITHDRAWAL"}
+]
+Output: { "A1": 500, "A2": -1 }
+ */
+
+
